46b1030ab9475df3457bfe53ace631f32b5c3f72
dennysapopescu <88237942+dennysapopescu@users.noreply.github.com>
2022-11-23 00:28:12 +0200

    Update saritura_calului.cpp

--- proiect/saritura_calului.cpp
+++ proiect/saritura_calului.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 using namespace std;
-
-int matr[100][100], n;
+int n;
+int matr[100][100]; 
 int linie_init = 0, coloana_init = 0;
 
 //directiile in care se deplaseaza calul pe tabla de sah
-int dl[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
-int dc[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+const int dir = 8;
+const int dl[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+const int dc[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
 void citire()
 {
 	cout << "numarul de linii si coloane: ";
 	cin >> n;
-	cout << "linia si coloana de la care porneste deplasarea: ";
+	cout << "linia si coloana de la care porneste deplasarea: " << endl;
 	cout << "linia: ";
 	cin >> linie_init;
 	cout << "coloana: ";
 	cin >> coloana_init;
 	//initializare matrice
-	matr[linie_init][coloana_init] = 0;
+	matr[linie_init][coloana_init] = 1;
 }
 
 bool plin()
@@ -29,6 +30,21 @@ bool plin()
 			if (matr[i][j] == 0)
 				return false;
 	return true;
+}
+
+void afisare()
+{
+	int i, j;
+	static int nr_sol = 1;
+	cout << "solutia cu numarul " << nr_sol << "\n";
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			cout << matr[i][j] << "\t";
+		cout << endl;
+	}
+	nr_sol++;
+	cout << "\n\n\n";
 }
 
 bool ok(int i, int j)
@@ -42,34 +58,20 @@ bool ok(int i, int j)
 	return true;
 }
 
-void afisare()
-{
-	int i, j;
-	int nr_sol = 1;
-	cout << "solutia cu numarul " << nr_sol << "\n";
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-			cout << matr[i][j] << "\t";
-		cout << endl;
-	}
-	nr_sol++;
-	cout << "\n\n\n";
-}
-
 void back(int l, int c)
 {
-	int i, vi, vj; //vecin i, vecin j
-	int k = 2;
+	int i;
+	static int k = 2;
 	if (plin())
 	{
 		afisare();
 		return;
 	}
-	for (i = 0; i < 8; i++)
+	else
+	for (i = 0; i < dir; i++)
 	{
-		vi = l + dl[i];
-		vj = c + dc[i];
+		int vi = l + dl[i];
+		int vj = c + dc[i];
 		if (ok(vi, vj))
 		{
 			matr[vi][vj] = k++;
